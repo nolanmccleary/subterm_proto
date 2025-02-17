4085401fe73d54a8896ea0f333f5b3701c1a76a7
nolan <nolancmccleary@gmail.com>
2025-02-17 08:39:44 -0800

    ascertained that only goofs use filters

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import whisper
 import torch
 
 
+#TODO: Find out how to get whisper model context length
+
 audio_source = Source_Handler()
 audio_preprocessor = Preprocessor(audio_source)
 audio_queue = Audio_Queue()
@@ -15,6 +17,10 @@ continue_recording = True
 transcript = ['']
 
 
+
+
+
+'''
 #TODO: If this works well, do it in CUDA
 FILTER_LEN = 3
 filter = [0.0, 0.0, 0.0]
@@ -29,26 +35,26 @@ def update_filter(val:float):
     return filter_avg
 
 
-
-
+#TODO: Redo this section and put the thing in jupyter
 if __name__ == "__main__":
     word_start = False
     word_end = False
-
+    result = []
     while True:
         try:
             (audio_frame, audio_confidence) = audio_preprocessor.process_chunk()
             audio_confidence = update_filter(audio_confidence)
+            result = result
             if audio_confidence > 0.5:
                 for val in audio_frame: #WILL PROBABLY NEED TO READJUST QUEUE SIZE
                     audio_queue.put(val)
+                result = whisper_model.transcribe(audio_queue.dump(), fp16=torch.cuda.is_available())
                 word_start = True
                 word_end = False
             else:
                 word_end = True if word_start else False
                 word_start = False
 
-            result = whisper_model.transcribe(audio_queue.dump(), fp16=torch.cuda.is_available())
             
             if word_end:
                 transcript.append(result)
@@ -58,4 +64,8 @@ if __name__ == "__main__":
 
     for line in transcript:
         print(line)
+
+'''
+
+
     
--- src/preprocessor/pylib/preprocessor.py
+++ src/preprocessor/pylib/preprocessor.py
@@ -5,18 +5,35 @@ from vad_handler.pylib import VAD_Handler
 
 class Preprocessor:
 
-    def __init__(self, source: Source_Handler):
+    def __init__(self, source: Source_Handler, filter_weights=[0.165, 0.165, 0.67]):
         self.source = source
         self.frame_handler = Frame_Handler()
         self.model_handler = VAD_Handler()
+        self.filter_weights = filter_weights
+        self.filter_len = len(self.filter_weights)
+        self.filter = [0 for _ in range(self.filter_len)]
+        self.unfiltered_confidence = 0        
 
 
-    def process_chunk(self):
+
+    def get_confidence(self):
         frame = self.frame_handler.get_frame(self.source.get_chunk())
         return (frame, self.model_handler.get_confidence(frame))
-    
 
 
 
+    #So far filtering seems useless
+    def get_filtered_confidence(self):
+        frame = self.frame_handler.get_frame(self.source.get_chunk())
+        confidence = self.model_handler.get_confidence(frame)
+        self.unfiltered_confidence = confidence
 
-
+        for i in range(1, self.filter_len-1):
+            self.filter[i-1] = self.filter_weights[i-1] * self.filter[i]
+        self.filter[self.filter_len-1] = self.filter_weights[self.filter_len-1] * confidence
+        filtered_confidence = 0
+        
+        for i in range(self.filter_len):
+            filtered_confidence += self.filter[i] * self.filter_weights[i]
+        
+        return (frame, filtered_confidence)
